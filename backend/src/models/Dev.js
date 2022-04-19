809b8c8b6c8a1878bf42de479ead98ade37124a2
Jose Mauro <z.mauro@live.com>
2022-04-19 12:47:28 -0300

    add: endpoint to create dev user

--- backend/src/models/Dev.js
+++ backend/src/models/Dev.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DevSchema = new Schema({
+const DevSchema = new mongoose.Schema({
     name: String,
     github_username: String,
     avatar_url: String,
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,33 @@
-const {Router} = require('express');
+const {Router, application} = require('express');
+const axios = require('axios');
+const Dev = require('./models/Dev')
 
 routes = Router()
 
-routes.post('/users', (request, response) => {
-    console.log(request.body);
-    return response.json({message: 'Hello world Dev Front-End!'});
+routes.post('/devs', async (request, response) => {
+    const { github_username, techs } = request.body;
+
+    const githubApiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
+    
+    var {name, login, avatar_url, bio} = githubApiResponse.data;
+
+    name = name ? name : login;
+
+    console.log("name ", name);
+
+    techsArray = techs.split(',').map(tech => tech.trim());
+    
+    const dev_obj = {
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+    }
+
+    const dev = await Dev.create(dev_obj);
+    
+    return response.json(dev);
 });
 
 module.exports = routes;
